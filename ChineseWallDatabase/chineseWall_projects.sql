d119e29ca44adb9a1ef17aa030608d38567a62af
GPrajwal12 <praj12121998@gmail.com>
2020-06-19 16:59:55 +0530

    database Updated

--- ChineseWallDatabase/chineseWall_projects.sql
+++ ChineseWallDatabase/chineseWall_projects.sql
@@ -32,7 +32,7 @@ CREATE TABLE `projects` (
   `end_date` date DEFAULT NULL,
   `consultantNo` int(11) DEFAULT NULL,
   PRIMARY KEY (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `projects` (
 
 LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (1,'Amazon','onlineShopping','ABC',9,'2020-06-08',NULL,1),(2,'Zomato','onlineFoodDelivery','XYZ',9,'2020-06-08',NULL,3),(3,'Swiggy','onlineFoodDelivery','abc',2,'2018-11-08','2019-11-08',2),(4,'UberEats','onlineFoodDelivery','abc',6,'2019-01-03','2020-01-03',2),(5,'Microsoft','computerServices','abc',8,'2018-07-06',NULL,3),(6,'Indigo','airlines','abc',7,'2018-09-07',NULL,3),(7,'Zomato','onlineFoodDelivery','acd',9,'2018-06-02','2020-01-04',3),(8,'Apple','computerServices','amc',8,'2018-06-05','2020-01-05',1),(9,'AirIndia','airlines','dya',6,'2020-01-10','2020-04-05',1),(11,'IBM','computerServices','cadfa',8,'2007-02-03','2012-04-08',1),(12,'BigBinary','computerServices','adda',2,'2020-06-02',NULL,4),(13,'Dunzo','onlineFoodDelivery','ewq',3,'2014-06-09','2016-05-07',1);
+INSERT INTO `projects` VALUES (1,'Amazon','onlineShopping','ABC',9,'2020-06-08',NULL,1),(2,'Zomato','onlineFoodDelivery','XYZ',9,'2020-06-08',NULL,3),(3,'Swiggy','onlineFoodDelivery','abc',2,'2018-11-08','2019-11-08',2),(4,'UberEats','onlineFoodDelivery','bcd',6,'2019-01-03','2020-01-03',2),(5,'Microsoft','computerServices','efg',8,'2018-07-06',NULL,3),(6,'Indigo','airlines','hij',7,'2018-09-07',NULL,3),(7,'Zomato','onlineFoodDelivery','acd',9,'2018-06-02','2020-01-04',3),(8,'Apple','computerServices','amc',8,'2018-06-05','2020-01-05',1),(9,'AirIndia','airlines','dya',6,'2020-01-10','2020-04-05',1),(11,'IBM','computerServices','cadfa',8,'2007-02-03','2012-04-08',1),(12,'BigBinary','computerServices','adda',2,'2020-06-02',NULL,4),(13,'Dunzo','onlineFoodDelivery','ewq',3,'2014-06-09','2016-05-07',1),(14,'Flipkart','onlineShopping','yuer',8,'2020-05-09','2020-06-02',1);
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-14 15:43:25
+-- Dump completed on 2020-06-19 16:58:11
